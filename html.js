a34404134315f0d33875331791a58cff95141e0d
Nazmus Sayad <online2sayad@gmail.com>
2021-12-15 09:45:00 +0600

    Hi

---
+++ html.js
@@ -0,0 +1,51 @@
+document.querySelector('body').innerHTML = `
+<!-------------->
+<main>
+  <div class="container">
+    <section id="ticTacToe">
+      <div class="item1">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item2">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item3">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item4">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item5">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item6">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item7">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item8">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+      <div class="item9">
+        <i class="ico fal far fa-circle"></i>
+        <i class="ico fal far fa-times"></i>
+      </div>
+    </section>
+  </div>
+</main>
+<section class="bottomCon">
+  <div class="msgBox">Don't try to be smart.</div>
+</section>
+
+<button onclick="startGame()">Play</button>
+<!-------------->
+`
